16763d78632de451439872b82f3f98ad7530579b
LGTRilex <successtwins4@gmail.com>
2022-09-15 00:11:32 -0700

    Learnt JS Today

---
+++ app.js
@@ -0,0 +1,14 @@
+// DRY= DO NOT REPEAT YOUR SELF
+let count = 2
+
+ while (count <=1) {
+    count = count + 1;
+  console.log (`this code runned`)
+ }
+
+ while (count 100) {
+    count = count +1
+    if (count = 100) {
+	console. log (`count`)
+    }
+}
